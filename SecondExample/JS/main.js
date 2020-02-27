8d0143059b60bc8e5d4891a11f66b95adbfe95d9
DanilSkyba <54021033+DanilSkyba@users.noreply.github.com>
2020-02-27 13:40:45 +0200

    Second commit

---
+++ SecondExample/JS/main.js
@@ -0,0 +1,9 @@
+$(function() {
+	$('.offer-content a').click(function(){
+		$('.triangels').css('width','250px').css('height','250px').css('background-size','100%')
+	});
+	$('.offer').click(function(){
+		$('.offer-content h1').css('font-size','50px').css('color','orange')
+	});
+});
+
